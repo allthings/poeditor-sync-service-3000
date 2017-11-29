8bb7ff575e52406d62d7d6d6e1c9c40b66b2adb3
Marco Lüthy <marco@luethy.net>
2017-11-29 17:34:06 +0100

    import Typescript boilerplate
    from https://github.com/allthings/serverless-boilerplates/tree/master/typescript

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # poeditor-sync-service-3000
-Sync project translations from the POEditor API and stick them into S3
+
+AWS Lambda/API Gateway service to sync project translations from the
+[POEditor](https://poeditor.com/) [API](https://poeditor.com/docs/api) and stick them into AWS S3.
+
+# Contents
+
+1. [Installation / Setup](#installation--setup)
+1. [Local Development](#local-development)
+1. [Testing](#testing)
+1. [Encrypting Secrets](#encrypting-secrets)
+1. [Deployment](#deployment)
+   1. [Staging](#staging)
+   1. [Prerelease](#prerelease)
+   1. [Production](#production)
+
+## Installation / Setup
+
+```sh
+yarn install
+```
+
+## Local Development
+
+Best practice is to develop locally using a TDD approach. The boilerplate includes sample tests on
+which you can build, including example of how to mock AWS services.
+
+Start the development environment with:
+
+```sh
+yarn dev
+```
+
+Try out the webhook with `curl`:
+
+```sh
+curl -s \
+  -X POST \
+  -H "content-type: application/json" \
+  -d '{"id":"s1d2f34","foo":"bar"}' \
+  http://localhost:3000/webhook | \
+  jq
+```
+
+## Testing
+
+```sh
+yarn test
+```
+
+or
+
+```sh
+yarn watch:test
+```
+
+## Encrypting Secrets
+
+In production, encrypt sensitive environment variables or other secret strings with KMS:
+
+```sh
+yarn encrypt-string "super secret string"
+```
+
+To deploy secrets as part of an environment variable, add it to `serverless.yml` like so:
+
+```yaml
+service:
+  name: ${self:custom.package.name}
+  awsKmsKeyArn: ${self:custom.package.config.awsKmsKeyArn} # use a custom kms key, defined in package.json
+
+provider:
+  name: aws
+  environment:
+    SUPER_SECRET: AQECAHj6Y8swFFZ8sg2A5LDTngYQ4IY...YtXTBbxtG0Z0wAQG7HuQ==
+```
+
+## Deployment
+
+### Staging
+
+```sh
+yarn deploy
+```
+
+### Prerelease
+
+```sh
+npm version prerelease
+```
+
+### Production
+
+```sh
+npm version [major|minor|patch]
+```
---
+++ src/handler.ts
@@ -0,0 +1,72 @@
+import handler from 'alagarr'
+// @TODO: aws-xray-sdk-core sucks. it's full of bloat.
+import * as AwsXray from 'aws-xray-sdk-core'
+import 'source-map-support/register'
+import kmsDecrypt from './utils/kms'
+
+const { STAGE = 'development', CDN_HOST_URL = '' } = process.env
+const IS_PRODUCTION = STAGE !== 'development'
+
+const handlerConfig = {
+  cspPolicies: {
+    'child-src': '*',
+    'connect-src': '*',
+    'default-src': "'self'",
+    'font-src': [
+      `'self'`,
+      'https://fonts.gstatic.com',
+      'https://netdna.bootstrapcdn.com/font-awesome/',
+      CDN_HOST_URL,
+    ].join(' '),
+    'frame-ancestors': "'self'",
+    'frame-src': '*',
+    'img-src': `* data: blob: ${CDN_HOST_URL}`,
+    'report-uri': '/csp-reports',
+    'script-src': [
+      `'self'`,
+      `'unsafe-inline'`,
+      `'unsafe-eval'`,
+      'https://*.google-analytics.com',
+      'https://*.google.com',
+      'https://*.gstatic.com',
+      'https://*.mxpnl.com/',
+      'https://mixpanel.com',
+      CDN_HOST_URL,
+    ].join(' '),
+    'style-src': [`'self'`, `'unsafe-inline'`, 'https://fonts.googleapis.com', CDN_HOST_URL].join(
+      ' '
+    ),
+  },
+  enableCompression: IS_PRODUCTION,
+  headers: {
+    'strict-transport-security': 'max-age=31536000; includeSubDomains; preload',
+  },
+}
+
+/*
+  Enable X-Ray in production
+  See the results here:
+  https://console.aws.amazon.com/xray/home#/service-map
+*/
+if (IS_PRODUCTION) {
+  // tslint:disable:no-expression-statement no-var-requires
+  AwsXray.captureHTTPsGlobal(require('http'))
+  AwsXray.captureHTTPsGlobal(require('https'))
+  // tslint:enable
+}
+
+// @TODO: remove type 'any'
+export default handler(async (request: any, response: any) => {
+  const SUPER_SECRET = await kmsDecrypt(process.env.SUPER_SECRET || '') // result gets cached :-)
+
+  const { body } = request
+
+  if (SUPER_SECRET !== process.env.SUPER_SECRET) {
+    // The rules are only disabled because this is an example and
+    // console.log-ing makes for a simple example. Don't copy me.
+    // tslint:disable-next-line:no-expression-statement no-console
+    console.log('SUPER_SECRET was decrypted.')
+  }
+
+  return response.json({ message: 'Hi.', body })
+}, handlerConfig)
